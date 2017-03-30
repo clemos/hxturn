425919c17cb7ab3041fac6324593e5528e1a2219
Clément Charmet <clement@secretmedia.com>
2017-03-30 11:10:28 +0200

    moved away peer test

--- test/js/node/webrtc/PeerConnection.hx
+++
@@ -1,6 +0,0 @@
-package js.node.webrtc;
-
-@:jsRequire('webrtc','RTCPeerConnection')
-extern class PeerConnection extends js.html.rtc.PeerConnection {
-    
-}
--- test/js/node/webrtc/SessionDescription.hx
+++
@@ -1,6 +0,0 @@
-package js.node.webrtc;
-
-@:jsRequire('webrtc','RTCSessionDescription')
-extern class SessionDescription extends js.html.rtc.SessionDescription {
-    
-}
